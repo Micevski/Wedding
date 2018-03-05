56ed9b34670e2d1d2f310a548aa83028703c247e
David <davidkocani123@gmail.com>
2018-03-05 01:42:02 +0100

    Log in And Register Look added

--- app/build/generated/source/r/debug/android/support/coreui/R.java
+++ app/build/generated/source/r/debug/android/support/coreui/R.java
@@ -8,15 +8,15 @@ package android.support.coreui;
 
 public final class R {
     public static final class attr {
-        public static final int font = 0x7f03008e;
-        public static final int fontProviderAuthority = 0x7f030090;
-        public static final int fontProviderCerts = 0x7f030091;
-        public static final int fontProviderFetchStrategy = 0x7f030092;
-        public static final int fontProviderFetchTimeout = 0x7f030093;
-        public static final int fontProviderPackage = 0x7f030094;
-        public static final int fontProviderQuery = 0x7f030095;
-        public static final int fontStyle = 0x7f030096;
-        public static final int fontWeight = 0x7f030097;
+        public static final int font = 0x7f03008f;
+        public static final int fontProviderAuthority = 0x7f030091;
+        public static final int fontProviderCerts = 0x7f030092;
+        public static final int fontProviderFetchStrategy = 0x7f030093;
+        public static final int fontProviderFetchTimeout = 0x7f030094;
+        public static final int fontProviderPackage = 0x7f030095;
+        public static final int fontProviderQuery = 0x7f030096;
+        public static final int fontStyle = 0x7f030097;
+        public static final int fontWeight = 0x7f030098;
     }
     public static final class bool {
         public static final int abc_action_bar_embed_tabs = 0x7f040000;
@@ -64,45 +64,45 @@ public final class R {
         public static final int notify_panel_notification_icon_bg = 0x7f070070;
     }
     public static final class id {
-        public static final int action_container = 0x7f08000e;
-        public static final int action_divider = 0x7f080010;
-        public static final int action_image = 0x7f080011;
-        public static final int action_text = 0x7f080017;
-        public static final int actions = 0x7f080018;
-        public static final int async = 0x7f08001e;
-        public static final int blocking = 0x7f080021;
-        public static final int chronometer = 0x7f080029;
-        public static final int forever = 0x7f080049;
-        public static final int icon = 0x7f08004d;
-        public static final int icon_group = 0x7f08004e;
-        public static final int info = 0x7f080051;
-        public static final int italic = 0x7f080052;
-        public static final int line1 = 0x7f080056;
-        public static final int line3 = 0x7f080057;
-        public static final int normal = 0x7f080065;
-        public static final int notification_background = 0x7f080066;
-        public static final int notification_main_column = 0x7f080067;
-        public static final int notification_main_column_container = 0x7f080068;
-        public static final int right_icon = 0x7f080072;
-        public static final int right_side = 0x7f080073;
-        public static final int text = 0x7f08009a;
-        public static final int text2 = 0x7f08009b;
-        public static final int time = 0x7f0800a1;
-        public static final int title = 0x7f0800a2;
+        public static final int action_container = 0x7f08000f;
+        public static final int action_divider = 0x7f080011;
+        public static final int action_image = 0x7f080012;
+        public static final int action_text = 0x7f080018;
+        public static final int actions = 0x7f080019;
+        public static final int async = 0x7f08001f;
+        public static final int blocking = 0x7f080023;
+        public static final int chronometer = 0x7f08002e;
+        public static final int forever = 0x7f08004f;
+        public static final int icon = 0x7f080053;
+        public static final int icon_group = 0x7f080054;
+        public static final int info = 0x7f080057;
+        public static final int italic = 0x7f080058;
+        public static final int line1 = 0x7f08005c;
+        public static final int line3 = 0x7f08005d;
+        public static final int normal = 0x7f08006c;
+        public static final int notification_background = 0x7f08006e;
+        public static final int notification_main_column = 0x7f08006f;
+        public static final int notification_main_column_container = 0x7f080070;
+        public static final int right_icon = 0x7f08007f;
+        public static final int right_side = 0x7f080080;
+        public static final int text = 0x7f0800a9;
+        public static final int text2 = 0x7f0800aa;
+        public static final int time = 0x7f0800b2;
+        public static final int title = 0x7f0800b3;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f090009;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a002a;
-        public static final int notification_action_tombstone = 0x7f0a002b;
-        public static final int notification_template_custom_big = 0x7f0a0032;
-        public static final int notification_template_icon_group = 0x7f0a0033;
-        public static final int notification_template_part_chronometer = 0x7f0a0037;
-        public static final int notification_template_part_time = 0x7f0a0038;
+        public static final int notification_action = 0x7f0a002e;
+        public static final int notification_action_tombstone = 0x7f0a002f;
+        public static final int notification_template_custom_big = 0x7f0a0036;
+        public static final int notification_template_icon_group = 0x7f0a0037;
+        public static final int notification_template_part_chronometer = 0x7f0a003b;
+        public static final int notification_template_part_time = 0x7f0a003c;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0c0032;
+        public static final int status_bar_notification_info_overflow = 0x7f0c0035;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f0d0100;
@@ -114,14 +114,14 @@ public final class R {
         public static final int Widget_Compat_NotificationActionText = 0x7f0d017f;
     }
     public static final class styleable {
-        public static final int[] FontFamily = { 0x7f030090, 0x7f030091, 0x7f030092, 0x7f030093, 0x7f030094, 0x7f030095 };
+        public static final int[] FontFamily = { 0x7f030091, 0x7f030092, 0x7f030093, 0x7f030094, 0x7f030095, 0x7f030096 };
         public static final int FontFamily_fontProviderAuthority = 0;
         public static final int FontFamily_fontProviderCerts = 1;
         public static final int FontFamily_fontProviderFetchStrategy = 2;
         public static final int FontFamily_fontProviderFetchTimeout = 3;
         public static final int FontFamily_fontProviderPackage = 4;
         public static final int FontFamily_fontProviderQuery = 5;
-        public static final int[] FontFamilyFont = { 0x7f03008e, 0x7f030096, 0x7f030097 };
+        public static final int[] FontFamilyFont = { 0x7f03008f, 0x7f030097, 0x7f030098 };
         public static final int FontFamilyFont_font = 0;
         public static final int FontFamilyFont_fontStyle = 1;
         public static final int FontFamilyFont_fontWeight = 2;
---
+++ app/src/main/java/com/example/dell/weeding/LoginRegister.java
@@ -0,0 +1,32 @@
+package com.example.dell.weeding;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+        TextView textView = (TextView)findViewById(R.id.register);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread registerThred = new Thread(){
+                    @Override
+                    public void run() {
+                        Intent regIntent = new Intent(getApplicationContext(),Register.class);
+                        startActivity(regIntent);
+                    }
+                };
+                registerThred.start();
+            }
+        });
+    }
+
+}
